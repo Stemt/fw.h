4018207f1358566f38c412d13d00480156aae382
Alaric de Ruiter <alaric0605@gmail.com>
2025-11-03 22:15:14 +0100

    changed the way the user recieves rename events

--- fw.h
+++ fw.h
@@ -3,13 +3,14 @@
 #include <string.h>
 #include <stdbool.h>
 
-#if defined __linux
+#if defined(__linux)
 #include <sys/inotify.h>
 #include <linux/limits.h>
 #include <unistd.h>
 #include <errno.h>
+#include <assert.h>
 #define FW_NAME_MAX NAME_MAX
-#elif defined __WIN32
+#elif defined(__WIN32)
 #include <windows.h>
 #include <fileapi.h>
 #include <shlwapi.h>
@@ -18,21 +19,11 @@
 #error "Platform not supported"
 #endif
 
-
 typedef enum{
-#if defined __linux
-  FW_CREATE     = IN_CREATE,
-  FW_DELETE     = IN_DELETE,
-  FW_MODIFY     = IN_MODIFY,
-  FW_MOVED_FROM = IN_MOVED_FROM,
-  FW_MOVED_TO   = IN_MOVED_TO,
-#elif defined __WIN32
-  FW_CREATE     = (1<<0),
-  FW_DELETE     = (1<<1),
-  FW_MODIFY     = (1<<2),
-  FW_MOVED_FROM = (1<<3),
-  FW_MOVED_TO   = (1<<4),
-#endif
+  FW_CREATE = (1<<0),
+  FW_DELETE = (1<<1),
+  FW_MODIFY = (1<<2),
+  FW_RENAME = (1<<3),
 } FW_Event;
 
 typedef enum{
@@ -44,6 +35,7 @@ typedef enum{
   FW_E_ACCESS_DENIED,
   FW_E_BAD_STATE,
   FW_E_NO_EVENT,
+  FW_E_INCOMPLETE_EVENT,
   FW_E_IO_ERROR,
 } FW_Error;
 
@@ -52,15 +44,19 @@ typedef struct{
   FW_Event watch_events;
   FW_Event received_events;
   char name[FW_NAME_MAX+1];
+  char new_name[FW_NAME_MAX+1];
+  
+  char event_buffer[1024];
 
-#if defined __linux
+#if defined(__linux)
   int fd;
   int wd;
+  int bytes_left;
+  struct inotify_event* event;
 
-#elif defined __WIN32
+#elif defined(__WIN32)
   HANDLE handle;
-  char change_buffer[1024];
-  FILE_NOTIFY_INFORMATION* next_event;
+  FILE_NOTIFY_INFORMATION* event;
   OVERLAPPED event_info;
 #endif
   
@@ -75,6 +71,7 @@ bool fw_once(FW* self, const char* path, FW_Event events);
 // --- event data getters ---
 FW_Event fw_event(FW* self);
 const char* fw_name(FW* self);
+const char* fw_new_name(FW* self);
 
 // --- error handling ---
 const char* fw_strerror(FW_Error error);
@@ -84,7 +81,7 @@ FW_Error fw_error(FW* self);
 bool fw_init(FW* self, const char* path, FW_Event events){
   self->watch_events = events;
 
-#if defined __linux
+#if defined(__linux)
 
   self->fd = inotify_init();
 
@@ -98,7 +95,13 @@ bool fw_init(FW* self, const char* path, FW_Event events){
     return false;
   }
 
-  self->wd = inotify_add_watch(self->fd, path, events);
+  int in_events = 0;
+  if(events & FW_CREATE) in_events |= IN_CREATE;
+  if(events & FW_DELETE) in_events |= IN_DELETE;
+  if(events & FW_MODIFY) in_events |= IN_MODIFY;
+  if(events & FW_RENAME) in_events |= IN_MOVE;
+
+  self->wd = inotify_add_watch(self->fd, path, in_events);
   if(self->wd < 0){
     switch(errno){
       case EACCES: self->error = FW_E_ACCESS_DENIED; break;
@@ -118,9 +121,9 @@ bool fw_init(FW* self, const char* path, FW_Event events){
   }
   return true;
 
-#elif defined __WIN32
+#elif defined(__WIN32)
 
-  self->next_event = NULL;
+  self->event = NULL;
   self->handle = CreateFile(strdup(path),
       FILE_LIST_DIRECTORY,
       FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
@@ -145,54 +148,141 @@ bool fw_init(FW* self, const char* path, FW_Event events){
 };
 
 void fw_deinit(FW* self){
-#if defined __linux
+#if defined(__linux)
   inotify_rm_watch(self->fd, self->wd);
   close(self->fd);
-#elif defined __WIN32
+#elif defined(__WIN32)
   CloseHandle(self->handle);
 #endif
 }
 
-bool fw_watch(FW* self){
-  self->received_events = 0;
-  memset(self->name, 0, sizeof(self->name));
+bool fw__event_queue_is_empty(FW* self){
+#if defined(__linux)
+  return self->bytes_left == 0;
+#elif defined(__WIN32)
+  return self->event == NULL;
+#endif
+}
 
-#if defined __linux
+void fw__consume_event(FW* self, char* name_buf){
+#if defined(__linux)
+  self->event = (struct inotify_event*) self->event_buffer;
+  if(name_buf != NULL){
+    memcpy(name_buf, self->event->name, self->event->len);
+  }
+  int event_size = sizeof(*self->event)+self->event->len;
+  self->bytes_left -= event_size;
+  assert(self->bytes_left >= 0);
+  for(int i = 0; i < self->bytes_left; ++i){
+    self->event_buffer[i] = self->event_buffer[i+event_size];
+  }
 
-  char buffer[sizeof(struct inotify_event) + NAME_MAX];
-  int n = read(self->fd, buffer, sizeof(buffer));
-  if(n < 0){
-    switch(errno){
-      case EAGAIN: self->error = FW_E_NO_EVENT; break;
-      case EACCES: self->error = FW_E_ACCESS_DENIED; break;
-      case EBADF:  self->error = FW_E_UNKNOWN; break;
-      case EFAULT: self->error = FW_E_BAD_STATE; break;
-      case EINTR:  self->error = FW_E_NO_EVENT; break;
-      case EINVAL: self->error = FW_E_BAD_STATE; break;
-      case EIO:    self->error = FW_E_IO_ERROR; break;
-      default:     self->error = FW_E_UNKNOWN; break;
+#elif defined(__WIN32)
+  if(name_buf != NULL){
+    // TODO: handle non ascii names
+    DWORD name_len = self->event->FileNameLength/sizeof(wchar_t);
+    for(DWORD i = 0; i < name_len; ++i){
+      name_buf[i] = self->event->FileName[i];
     }
+  }
+
+  if(self->event->NextEntryOffset != 0){
+    self->event = (FILE_NOTIFY_INFORMATION*)(((char*)self->event) + self->event->NextEntryOffset);
+  }else{
+    self->event = NULL;
+  }
+#endif
+}
+
+bool fw_watch(FW* self){
+  if(self->watch_events == 0){
+    self->error = FW_E_NO_EVENT;
     return false;
   }
-  struct inotify_event* event = (void*)buffer;
-  self->received_events = event->mask & self->watch_events;
-  memcpy(self->name, event->name, event->len);
 
-  return true;
-#elif defined __WIN32
+  self->received_events = 0;
+  memset(self->name, 0, sizeof(self->name));
+  memset(self->new_name, 0, sizeof(self->new_name));
+
+#if defined(__linux)
+
+  while(self->received_events == 0){
+    if(self->bytes_left == 0){
+      int n = read(self->fd, self->event_buffer, sizeof(self->event_buffer));
+      if(n < 0){
+        switch(errno){
+          case EAGAIN: self->error = FW_E_NO_EVENT; break;
+          case EACCES: self->error = FW_E_ACCESS_DENIED; break;
+          case EBADF:  self->error = FW_E_UNKNOWN; break;
+          case EFAULT: self->error = FW_E_BAD_STATE; break;
+          case EINTR:  self->error = FW_E_NO_EVENT; break;
+          case EINVAL: self->error = FW_E_BAD_STATE; break;
+          case EIO:    self->error = FW_E_IO_ERROR; break;
+          default:     self->error = FW_E_UNKNOWN; break;
+        }
+        return false;
+      }
+      self->bytes_left = n;
+    }
+
+    while(self->bytes_left > 0){
+      struct inotify_event* event = (void*)self->event_buffer;
+      switch(event->mask){
+        case IN_CREATE:
+          self->received_events = FW_CREATE;
+          fw__consume_event(self, self->name);
+          return true;
+        case IN_DELETE:
+          self->received_events = FW_DELETE;
+          fw__consume_event(self, self->name);
+          return true;
+        case IN_MODIFY:
+          self->received_events = FW_MODIFY;
+          fw__consume_event(self, self->name);
+          return true;
+        case IN_MOVED_FROM:
+          fw__consume_event(self, self->name);
+          if(strlen(self->new_name) > 0){
+            self->received_events = FW_RENAME;
+            return true;
+          }else if(fw__event_queue_is_empty(self)){
+            self->received_events = FW_RENAME;
+            // new name was not received but
+            // no more events are available
+            self->error = FW_E_INCOMPLETE_EVENT;
+            return true;
+          }
+          break;
+        case IN_MOVED_TO:
+          fw__consume_event(self, self->new_name);
+          if(strlen(self->name) > 0){
+            self->received_events = FW_RENAME;
+            return true;
+          }else if(fw__event_queue_is_empty(self)){
+            self->received_events = FW_RENAME;
+            // old name was not received but
+            // no more events are available
+            self->error = FW_E_INCOMPLETE_EVENT;
+            return true;
+          }
+          break;
+        default: break;
+      }
+    }
+  }
+  return false;
+#elif defined(__WIN32)
 
   self->received_events = 0;
 
-  while((self->received_events & self->watch_events) == 0){
-
-    FILE_NOTIFY_INFORMATION* event = (FILE_NOTIFY_INFORMATION*)self->change_buffer;
+  while(true){
 
     // get next event
-    if(self->next_event == NULL){
+    if(self->event == NULL){
       DWORD ret = ReadDirectoryChangesW(
           self->handle,
-          self->change_buffer,
-          sizeof(self->change_buffer),
+          self->event_buffer,
+          sizeof(self->event_buffer),
           TRUE,
           FILE_NOTIFY_CHANGE_FILE_NAME
           | FILE_NOTIFY_CHANGE_DIR_NAME
@@ -200,6 +290,7 @@ bool fw_watch(FW* self){
           NULL,
           &self->event_info,
           NULL);
+        
       if(ret == 0){
         self->error = FW_E_UNKNOWN;
         return false;
@@ -214,46 +305,74 @@ bool fw_watch(FW* self){
         }
         return false;
       }
-    }else{
-      event = self->next_event;
+      self->event = (FILE_NOTIFY_INFORMATION*)self->event_buffer;
     }
 
     DWORD n = 0;
     GetOverlappedResult(self->handle, &self->event_info, &n, FALSE);
-    FILE_NOTIFY_INFORMATION* old_event = NULL;
-    char* name_buf = self->name;
-    switch(event->Action){
+    switch(self->event->Action){
       case FILE_ACTION_ADDED:
-        self->received_events |= FW_CREATE;
+        if(self->watch_events & FW_CREATE){
+          self->received_events = FW_CREATE;
+          fw__consume_event(self, self->name);
+          return true;
+        }else{
+          fw__consume_event(self, NULL);
+        }
         break;
       case FILE_ACTION_REMOVED:
-        self->received_events |= FW_DELETE;
+        if(self->watch_events & FW_DELETE){
+          self->received_events = FW_DELETE;
+          fw__consume_event(self, self->name);
+          return true;
+        }else{
+          fw__consume_event(self, NULL);
+        }
         break;
       case FILE_ACTION_MODIFIED:
-        self->received_events |= FW_MODIFY;
+        if(self->watch_events & FW_MODIFY){
+          self->received_events = FW_MODIFY;
+          fw__consume_event(self, self->name);
+          return true;
+        }else{
+          fw__consume_event(self, NULL);
+        }
         break;
       case FILE_ACTION_RENAMED_OLD_NAME:
-        self->received_events |= FW_MOVED_FROM;
+        if(self->watch_events & FW_RENAME){
+          self->received_events = FW_RENAME;
+          fw__consume_event(self, self->name);
+          if(strlen(self->new_name) > 0){
+            self->received_events = FW_RENAME;
+            return true;
+          }else if(fw__event_queue_is_empty(self)){
+            // new name was not received but
+            // no more events are available
+            self->error = FW_E_INCOMPLETE_EVENT;
+            return true;
+          }
+        }else{
+          fw__consume_event(self, NULL);
+        }
         break;
       case FILE_ACTION_RENAMED_NEW_NAME:
-        self->received_events |= FW_MOVED_TO;
+        if(self->watch_events & FW_RENAME){
+          self->received_events = FW_RENAME;
+          fw__consume_event(self, self->new_name);
+          if(strlen(self->name) > 0){
+            return true;
+          }else if(fw__event_queue_is_empty(self)){
+            // old name was not received but
+            // no more events are available
+            self->error = FW_E_INCOMPLETE_EVENT;
+            return true;
+          }
+        }else{
+          fw__consume_event(self, NULL);
+        }
         break;
       default: break;
     }
-    
-    // TODO: handle non ascii names
-    DWORD name_len = event->FileNameLength/sizeof(wchar_t);
-    for(DWORD i = 0; i < name_len; ++i){
-      name_buf[i] = event->FileName[i];
-    }
-
-    if(event->NextEntryOffset != 0){
-      self->next_event = (FILE_NOTIFY_INFORMATION*)(((char*)event) + event->NextEntryOffset);
-    }else{
-      self->next_event = NULL;
-    }
-
-    self->received_events &= self->watch_events;
   }
 
   return true;
@@ -271,6 +390,7 @@ const char* fw_strerror(FW_Error error){
     case FW_E_PATH_NOT_FOUND: return "Path not found"; 
     case FW_E_PATH_TOO_LONG: return "Path is too long"; 
     case FW_E_UNKNOWN: return "An unknown error occured"; 
+    case FW_E_INCOMPLETE_EVENT: return "An incomplete (rename) event was recieved"; 
   }
   return "Invalid error code provided to fw_strerror";
 }
@@ -285,6 +405,10 @@ FW_Event fw_event(FW* self){
 
 const char* fw_name(FW* self){
   return self->name;
+}
+
+const char* fw_new_name(FW* self){
+  return self->new_name;
 }
 
 bool fw_once(FW* self, const char* path, FW_Event events){
